41eb5cd4e1fc85b7973e1186bfecd9ac5b34e84b
Palotai Agnes <palotaiagnes@freemail.hu>
2019-05-15 22:21:59 +0200

    A StringEllenorzo osztály teszelése.

---
+++ src/test/java/model/StringEllenorzoTest.java
@@ -0,0 +1,66 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringEllenorzoTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void stringEllenorzese() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void aBeirtKarakterekSzamaNulla() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void aBeirtKarakterekSzamaMaxTizennyolcLehet() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void nemMegengedettKaraktertTartalmaz() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void azEgyHosszusaguHelyrajziSzamCsakNemNullaSzamLehet() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void nemKezdodhetKetNullaval() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void ketPerjelNemLehetEgymasMellett() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void maxHaromPerjelLehetBenne() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void nemVegzodhetPerjellel() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void betuEsSzamjegyNemLehetEgymasMellett() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void karakterTipusaPerjel() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void karakterTipusaSzamjegy() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void karakterTipusaMegengedettBetu() {
+    }
+}
