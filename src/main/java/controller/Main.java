d3166dc7ff9aed92e81e0dfb1702b5575908e1e9
Palotai Agnes <palotaiagnes@freemail.hu>
2019-05-15 21:22:59 +0200

    A Main.class létrehozása.

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -1,4 +1,39 @@
 package controller;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import model.*;
+
+
+/**
+ * Helyrajzi-szám ellenőrző program.
+ * A felhasználótól bekér egy sztringet, majd megvizsgálja, hogy
+ * az megfelel-e egy helyrajzi-szám formátumának.
+ * Ha nem felel meg, akkor a hiba okának megfelelő üzenetet ír ki.
+ * Ha megfelel, akkor megkeresi egy adatbázisban, hogy van-e
+ * a helyrajzi-számhoz megállapított cím.
+ * @author Palotai Ágnes
+ * @version 1.0
+ */
+public class Main extends Application {
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/view.fxml"));
+        primaryStage.setTitle("Helyrajzi-szám ellenőrzése");
+        primaryStage.setResizable(false);
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
 }
