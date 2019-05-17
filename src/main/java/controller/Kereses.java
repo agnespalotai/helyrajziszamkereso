411f64cfc1d2416deadc9904b42285ff2de93999
Palotai Agnes <palotaiagnes@freemail.hu>
2019-05-17 17:08:29 +0200

    Kereses osztaly módosítása.

---
+++ src/main/java/controller/Kereses.java
@@ -0,0 +1,28 @@
+package controller;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+import model.*;
+
+public class Kereses {
+
+    private static EntityManager em;
+
+    public List<Hrsz> readHrszBy(String hrsz){
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hrsz_remotemysql");
+        em = emf.createEntityManager();
+
+        TypedQuery<Hrsz> q = em.createQuery("SELECT h FROM Hrsz h WHERE h.helyrajziszam='" + hrsz + "'", Hrsz.class);
+        List<Hrsz> result = q.getResultList();
+
+        em.close();
+        emf.close();
+        return result;
+    }
+
+}
